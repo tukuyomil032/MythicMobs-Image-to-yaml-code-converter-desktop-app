4598e924c3a978deaffdcc8ce379cf5b5590c515
tukuyomi032 <tukuyomi032@gmail.com>
2025-11-23 04:26:38 +0900

    Removed unnecessary comments from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-// vite.config.ts (新規作成 または 修正)
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  
-  // ▼▼▼ GitHub Pagesのためにこの設定を追加 ▼▼▼
-  // 'YOUR_REPOSITORY_NAME' の部分を、あなたのGitHubリポジトリ名に書き換えてください。
-  // (例: 'https://YourName.github.io/mm-image-converter/' のようにするため)
   base: 'MythicMobs-Image-to-yaml-code-converter', 
-  // ▲▲▲ GitHub Pagesのためにこの設定を追加 ▲▲▲
 })
 
