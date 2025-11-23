f3418195ed9a5d0d97d7517546f393e0015e96cb
tukuyomi032 <tukuyomi032@gmail.com>
2025-11-23 15:17:33 +0900

    feat: Add Discord Rich Presence

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const fs = require('fs');
+const Rpc = require('discord-rpc');
+const isDev = !app.isPackaged;
+const clientId = '1442024361036087296'; 
 
-const isDev = !app.isPackaged; 
+
+Rpc.register(clientId);
+const rpc = new Rpc.Client({ transport: 'ipc' });
+const startTimestamp = new Date(); 
+
+async function setActivity() {
+  if (!rpc || !win) {
+    return;
+  }
+
+  
+  rpc.setActivity({
+    details: 'Playing Image Converter',     
+    state: 'Configuring settings for code generation...', 
+    startTimestamp,                    
+    largeImageKey: 'mm_logo', 
+    largeImageText: 'MythicMobs - Image to Yaml Code Converter', 
+    smallImageKey: 'app_icon', 
+    smallImageText: 'v0.1.0', 
+    instance: false,
+  });
+}
+
+let win; 
 
 function createWindow() {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({ 
     width: 1200,
     height: 900,
     webPreferences: {
@@ -28,10 +54,21 @@ if (!fs.existsSync(preloadPath)) {
   fs.writeFileSync(preloadPath, '');
 }
 
+app.whenReady().then(() => {
+  createWindow(); 
 
-app.whenReady().then(createWindow);
+  rpc.on('ready', () => {
+    console.log('Discord Rich Presence is ready.');
+    setActivity();
+  });
+
+  rpc.login({ clientId }).catch(err => {
+    console.error('Failed to login to Discord RPC:', err);
+  });
+});
 
 app.on('window-all-closed', () => {
+  rpc.destroy(); 
   if (process.platform !== 'darwin') {
     app.quit();
   }
